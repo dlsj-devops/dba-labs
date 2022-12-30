873aa55d143589d935f4787a33efb2bd45fae031
Daniel de Lima <dlsj.devops@gmail.com>
2022-12-30 09:33:22 -0300

    oracle - habilitando fra e archivemode

---
+++ oracle/backup_recovery/README.md
@@ -0,0 +1,22 @@
+# descrição
+
+Material relacionado com backup e recovery.
+
+## Ambiente
+
+As práticas foram realizadas no ambiente de homologação do local de trabalho.
+
+Dados do ambiente do local de trabalho:
+
+Oracle Database 12c Enterprise Edition Release 12.2.0.1.0 - 64bit Production
+Red Hat Enterprise Linux Server release 7.6 (Maipo)
+
+## Fontes
+
+As fontes utilizadas:
+
+- https://www.youtube.com/watch?v=cfSEceulcWQ
+
+- https://docs.oracle.com/en/database/oracle/oracle-database/12.2/bradv/introduction-backup-recovery.html#GUID-9997EF87-B293-44D4-92F3-DD938E79170D
+
+
---
+++ oracle/backup_recovery/habilitando_fra_archive_mode.sql
@@ -0,0 +1,48 @@
+--consultando os parâmetros da FRA
+
+set line 150;
+
+show parameter db_recovery_file_dest;
+
+--configurar na sequencia
+
+--primeiro db_recovery_file_dest_size
+alter system set db_recovery_file_dest_size=50G scope=BOTH;
+
+--segundo db_recovery_file_dest
+alter system set db_recovery_file_dest = '/backup/pa6hom/fra' scope=both;
+
+--configurando o flashback log e o tempo de retenção
+
+select flashback_on from v$database; --veio NO
+
+alter database flashback on;
+
+--gerou os erros:
+--ORA-38706: Cannot turn on FLASHBACK DATABASE logging.
+--ORA-38707: Media recovery is not enabled (banco não está em archive log)
+
+--verifique se o banco está com archive log habilitado
+archive log list; --retorna No Archive Mode
+
+shutdown immediate;
+
+startup mount;
+
+alter database archivelog;
+
+alter database open;
+
+--tentar novamente
+alter database flashback on;
+
+--tempo de retenção
+alter system set db_flashback_retention_target=1440 scope=both;
+
+--consultando as informações da FRA
+
+select * from v$flashback_database_log; 
+
+
+
+
